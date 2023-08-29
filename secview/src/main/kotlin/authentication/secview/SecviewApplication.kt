7758599da6c5dda1134faabb0cef6b53d9677e39
Heitor <heitorm0rei1@gmail.com>
2023-08-29 00:25:35 -0300

    adding kotlin authentication project

---
+++ secview/src/main/kotlin/authentication/secview/SecviewApplication.kt
@@ -0,0 +1,11 @@
+package authentication.secview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecviewApplication
+
+fun main(args: Array<String>) {
+	runApplication<SecviewApplication>(*args)
+}
---
+++ secview/src/test/kotlin/authentication/secview/SecviewApplicationTests.kt
@@ -0,0 +1,13 @@
+package authentication.secview
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SecviewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
