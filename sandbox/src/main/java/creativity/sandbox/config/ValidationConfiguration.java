8a54749e4f03742def8195a3608d1bb878f42b85
HectMo <heitorm0rei1@gmail.com>
2025-09-02 23:14:06 -0300

    commit for cherrypick

--- sandbox/src/main/java/creativity/sandbox/config/ValidationConfiguration.java
+++ sandbox/src/main/java/creativity/sandbox/config/ValidationConfiguration.java
@@ -11,6 +11,7 @@ import java.util.Locale;
 
 @Configuration
 public class ValidationConfiguration {
+    //Add more files configurations/jwt and message streaming
 
     @PostConstruct
     public void init() {
